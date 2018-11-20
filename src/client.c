4790b0ab23cd10b93aace39c89deac03e87c061b
xoreo <mattnappo@gmail.com>
2018-11-20 18:32:40 -0500

    might need threading

--- src/client.c
+++ src/client.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -13,6 +13,11 @@
 
 int main() {
     // Raw sockets are independent of an actual protocol - might be useful later
+    // This program uses TCP
+    
+    // Main loop bool
+    int loop = 0;
+
     // Create a socket
     int client_socket;
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -21,7 +26,8 @@ int main() {
     struct sockaddr_in server_address;
     server_address.sin_family = INADDR_ANY;
     server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr(IP);
+    // server_address.sin_addr.s_addr = inet_addr(IP);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
     // Make the connection to another socket
     int connection_status = connect(
@@ -29,22 +35,30 @@ int main() {
       (struct sockaddr *) &server_address,
       sizeof(server_address)
     );
-
+    
     if (connection_status == -1) {
         printf("unable to create connection to server\n");
         printf("error code: %i\n", connection_status);
+    } else {
+        printf("connected to the server socket\n");
+        loop = 1;
     }
 
     // Main loop
-    while (1) {
+    while (loop) {
+        printf("RUNNING MAIN LOOP\n\n");
+        // Ask user for input
+        char message[256];
+        printf("message: ");
+        scanf("%s", message);
+        printf("\n");
+
         // Recieve data from the server
-        char server_response[256];
-        recv(client_socket, &server_response, sizeof(server_response), 0);
-        printf("server: %s\n", server_response);
+        // char server_response[256];
+        // recv(client_socket, &server_response, sizeof(server_response), 0);
+        // printf("server: %s\n", server_response);
 
         // Send data to the server
-        char message[256];
-        printf("message: %s", message);
         send(client_socket, message, sizeof(message), 0);
     }
     
