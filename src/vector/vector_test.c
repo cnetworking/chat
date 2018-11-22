cc11c7aae6d8d9378fd0ae41fbf671ed8b2ffcb0
xoreo <mattnappo@gmail.com>
2018-11-22 12:24:43 -0500

    reverted changes

--- src/vector/vector_test.c
+++ src/vector/vector_test.c
@@ -3,21 +3,21 @@
 
 int main() {
 	// declare and initialize a new vector
-	Vector vector;
-	vector_init(&vector);
+	Vector *vector;
+	vector_init(vector);
 
 	// fill it up with 150 arbitrary values
 	// this should expand capacity up to 200
 	for (int i = 0; i < 20; i++) {
-		vector_append(&vector, i);
+		vector_append(vector, i);
 	}
 
 	// print out an arbitrary value in the vector
 	for (int i = 0; i < 20; i++) {
-		printf("%i\n", vector_get(&vector, i));	
+		printf("%i\n", vector_get(vector, i));
 	}
 
 	// we're all done playing with our vector, 
 	// so free its underlying data array
-	vector_free(&vector);
+	vector_free(vector);
 }
