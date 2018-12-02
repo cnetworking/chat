72297205e9a095b96c6b76709393214554c62f1b
xoreo <mattnappo@gmail.com>
2018-12-02 18:41:25 -0500

    fixed list.h

--- src/list/list.h
+++ src/list/list.h
@@ -1,31 +1,31 @@
 #ifndef LIST_H
 #define LIST_H
 
-    typedef struct NodeTag {
-        char *data;
-        struct NodeTag *next;
-    } Node;
+typedef struct NodeTag {
+    char *data;
+    struct NodeTag *next;
+} Node;
 
-    Node *Node_create();
-    void Node_destroy(Node *node);
+Node *Node_create();
+void Node_destroy(Node *node);
 
-    typedef struct ListTag {
-        struct NodeTag *first;
-    } List;
+typedef struct ListTag {
+    struct NodeTag *first;
+} List;
 
-    List *List_create();
-    void List_destroy(List *list);
+List *List_create();
+void List_destroy(List *list);
 
-    void List_append(List *list, char *str);
-    void List_insert(List *list, int index, char *str);
+void List_append(List *list, char *str);
+void List_insert(List *list, int index, char *str);
 
-    char *List_get(List *list, int index);
-    int List_find(List *list, char *str);
+char *List_get(List *list, int index);
+int List_find(List *list, char *str);
 
-    void List_remove(List *list, int index);
-    char *List_pop(List *list, int index);
+void List_remove(List *list, int index);
+char *List_pop(List *list, int index);
 
-    int List_length(List *list);
-    void List_print(List *list);
+int List_length(List *list);
+void List_print(List *list);
 
 #endif
